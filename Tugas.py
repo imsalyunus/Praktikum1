53d2b305f1f81cbacd32e6ab14674ad9815c331e
imsalyunus <92619516+imsalyunus@users.noreply.github.com>
2021-10-27 23:54:35 +0700

    Add files via upload

---
+++ Tugas.py
@@ -0,0 +1,19 @@
+print("=====TUGAS====")
+print("=====MENGINPUT BIODATA DIRI=====")
+
+nama =input("nama:")
+call =input("panggilan:")
+npm =input("npm:")
+ttl =input("ttl:")
+umur =input("umur:")
+alamat =input("alamat:")
+telepon =input("telepon:")
+
+print("   assalamu'alaikum Wr.Wb")
+
+print("Let me introduce my self. My Name is ",nama,", but you can call me ",call,"."
+"My NPM is ",npm,". i was born in ",ttl,"and i am",umur,"years old. "
+"i am very glad if you want to inivite my house in ",alamat,".so, don't forget to call me"
+"before with the number",telepon,".")
+
+print ("thank you")
---
+++ latihan1.py
@@ -0,0 +1,20 @@
+print("hello")
+print("saya sedang belajar python")
+
+a= 8
+b= 6
+print ("variabel a=",a)
+print ("variable b=",b)
+print ("hasil penjumlahan a+b=",a+b)
+
+a=input("masukkan nilai a:")
+b=input("masukkan nilai b:")
+print("variabel a=",a)
+print("variabel b=",b)
+print("hasil penggabungan {1}&{0}=%d".format(10,5) %(10+5))
+
+#konvensi nilai variable
+a=int(a)
+b=int(b)
+print("hasil penjumlahan {1}+{0}=%d".format(a,b) %(a+b))
+print("hasil pembagian {1}/{0}=%d".format(a,b)%(a/b))
